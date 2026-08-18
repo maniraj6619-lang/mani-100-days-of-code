f4a25c14aba6f98409ddde006fa548b82779114e
maniraj6619-lang <maniraj6619@gmail.com>
2026-08-18 22:15:18 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,8 @@
+/*Q1: Write a prog input two numbers and display their sum.*/
+#include <stdio.h>
+void main(){
+    int a , b  , sum;
+    scanf("%d %d",&a,&b);
+    sum = a + b;
+    printf("%d",sum);
+}
