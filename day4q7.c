5cd7c87d73151c6ba87cb6d6abc4c9039ffedb52
maniraj6619-lang <maniraj6619@gmail.com>
2026-09-22 23:01:15 +0530

    Add files via upload

---
+++ day4q7.c
@@ -0,0 +1,8 @@
+/*Write a program to swap two numbers without using a third variable.*/
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("SPECIFY YOUR NUMBERS:\t");
+    scanf("%d %d",&a,&b);
+    printf("HERES YOUR NUMBERS:%d %d",b,a);
+}
