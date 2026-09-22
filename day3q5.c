7d9ffd23d6f7095855a593d973fb48e1f9efed54
maniraj6619-lang <maniraj6619@gmail.com>
2026-09-22 22:56:46 +0530

    Add files via upload

---
+++ day3q5.c
@@ -0,0 +1,10 @@
+/*Write a program to convert temperature from Celsius to Fahrenheit.*/
+#include <stdio.h>
+void main(){
+    float a,b;
+    printf("TEMPERATURE IN CELSIUS:");
+    scanf("%f",&a);
+    b= a*9/5+32;
+    printf("YOUR TEMP IN FARENHEIT:%f",b);
+}
+    
