2d5ea53701bab00407e7799d9e0f5a58e283406f
maniraj6619-lang <maniraj6619@gmail.com>
2026-09-22 23:03:00 +0530

    Add files via upload

---
+++ day4q8.c
@@ -0,0 +1,10 @@
+/*Write a program to find and display the sum of the first n natural numbers.*/
+#include <stdio.h>
+void main(){
+    int a,c;
+    printf("ENTER THE NUMBER:");
+    scanf("%d", &a);
+    printf("%d", a*(a+1)/2);
+}
+
+    
