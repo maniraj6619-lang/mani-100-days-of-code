cf99a6f5c6ecf18b2b955211bf2ca47b213727db
maniraj6619-lang <maniraj6619@gmail.com>
2026-09-22 23:06:22 +0530

    Add files via upload

---
+++ day6q11.c
@@ -0,0 +1,11 @@
+/*write a program to input an integer and check whether it is even or odd using if-else*/
+#include <stdio.h>
+void main(){
+    int a;
+    printf("WHAT IS YOUR NUMBER:");
+    scanf("%d",&a);
+    if (a%2==0) 
+    printf("EVEN");
+    else 
+    printf("ODD");
+}
